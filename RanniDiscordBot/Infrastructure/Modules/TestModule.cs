bdcb1d966e1203adbae5f1bf9f2911248a89b5f2
M0o4 <indrei457@gmail.com>
2022-09-04 13:27:03 +0300

    Condition access

--- RanniDiscordBot/Infrastructure/Modules/TestModule.cs
+++ RanniDiscordBot/Infrastructure/Modules/TestModule.cs
@@ -9,9 +9,9 @@ public class TestModule : ModuleBase<SocketCommandContext>
     [Summary("test module")]
     public async Task SayAsync()
     {
-        await ReplyAsync($"{Context.Guild.Roles.FirstOrDefault(r => r.Name == "1 курс").Name}");
-        await (Context.User as IGuildUser).AddRoleAsync(
-            Context.Guild.Roles.FirstOrDefault(r => r.Name == "1 курс"));
+        await ReplyAsync($"{Context.Guild.Roles.FirstOrDefault(r => r.Name == "1 курс")?.Name}");
+        await (Context.User as IGuildUser)?.AddRoleAsync(
+            Context.Guild.Roles.FirstOrDefault(r => r.Name == "1 курс"))!;
         await ReplyAsync("test");
     }
 }
