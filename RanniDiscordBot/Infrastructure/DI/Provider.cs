92223c34ef28fb66f1c8bedf0b21bc330bc811e4
M0o4 <indrei457@gmail.com>
2022-09-05 11:42:30 +0300

    Add icon and create Config

--- RanniDiscordBot/Infrastructure/DI/Provider.cs
+++ RanniDiscordBot/Infrastructure/DI/Provider.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using RanniDiscordBot.RanniDiscordBot.Configuration;
@@ -12,13 +13,32 @@ public class Provider
 {
     public ServiceProvider ConfigureServices()
     {
+        var client = CreateClient();
+
         return new ServiceCollection()
             .AddSingleton<IServer, Server>()
             .AddSingleton<ILogger, Logger>()
-            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton(client)
             .AddSingleton<IInteractiveService, InteractiveService>()
             .AddSingleton<CommandService>()
             .AddSingleton<ICommandHandler, CommandHandler>()
             .BuildServiceProvider();
+    }
+
+    private static DiscordSocketClient CreateClient()
+    {
+        var config = new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.DirectMessages | GatewayIntents.DirectMessages | GatewayIntents.GuildBans |
+                             GatewayIntents.GuildIntegrations | GatewayIntents.GuildMembers |
+                             GatewayIntents.GuildMessageReactions | GatewayIntents.GuildMessages | GatewayIntents.Guilds,
+            MessageCacheSize = 15,
+            LogLevel = LogSeverity.Info,
+            ConnectionTimeout = int.MaxValue,
+            AlwaysDownloadUsers = true,
+        };
+
+        var client = new DiscordSocketClient(config);
+        return client;
     }
 }
