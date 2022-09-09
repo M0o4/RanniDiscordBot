7cbff1b8b0d086560fced8cc85f7a696e25e6cee
M0o4 <indrei457@gmail.com>
2022-09-09 12:50:02 +0300

    Add MAUI Project

---
+++ RanniGUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RanniGUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
